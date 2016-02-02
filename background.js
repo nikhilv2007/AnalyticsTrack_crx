7521be3afcd0cad9eca58fcce5c13cfc44ced32a
nikhilv2007 <nikhilv_2007@yahoo.co.in>
2016-02-03 03:41:38 +0530

    fixed badge count for new tabs

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 //var analyticsRequest = 0;
 
 var data = [
-	{"urlPattern":"google-analytics", "provider": "Google Analytics", "events": [{"eventPattern": "analytics.js", "event": "Initialisation"},{"eventPattern": "ga.js", "event": "Initialisation"},{"eventPattern": "t=pageview", "event": "Page view"},{"eventPattern": "t=event", "event": "Event"},{"eventPattern": "t=timing", "event": "Timing"},{"eventPattern": "ec=ElementClick", "event": "Click"},{"eventPattern": "ec=performance", "event": "Timing"}]},
-	{"urlPattern":"analytics.yahoo", "provider": "Yahoo Analytics"},
+	{"urlPattern":"google-analytics", "provider": "Google Analytics", "events": [{"eventPattern": "analytics.js", "event": "Initialisation"},{"eventPattern": "ga.js", "event": "Initialisation"},{"eventPattern": "t=pageview", "event": "Page view"},{"eventPattern": "t=event", "event": "Event"},{"eventPattern": "t=timing", "event": "Timing"},{"eventPattern": "t=social", "event": "Social interaction"},{"eventPattern": "t=exception", "event": "Exception"},{"eventPattern": "t=screenview", "event": "Screen view"}]},
+	{"urlPattern":"analytics.yahoo", "provider": "Yahoo Analytics", "events": [{"eventPattern": "x=12", "event": "Click"}, {"eventPattern": "x=115", "event": "Hover"}]},
 	{"urlPattern":"googletagmanager", "provider": "Google Tag Manager", "events": [{"eventPattern": "gtm.js", "event": "Initialisation"}]},
 	{"urlPattern":"facebook.com/tr", "provider": "Facebook", "events": [{"eventPattern": "ev=PixelInitialized", "event": "Initialisation"},{"eventPattern": "ev=PageView", "event": "Page view"},{"eventPattern": "ev=ViewContent", "event": "Content view"}]},
-	{"urlPattern":"doubleclick.net", "provider": "DoubleClick", "events": [{"eventPattern": "dc.js", "event": "Initialisation"}]}
+	{"urlPattern":"doubleclick.net", "provider": "DoubleClick", "events": [{"eventPattern": "dc.js", "event": "Initialisation"},{"eventPattern": "ad_status.js", "event": "Initialisation"},{"eventPattern": "label=followon_view", "event": "View"}]},
+    {"urlPattern":"youtube.com/api/stats/", "provider": "Youtube", "events": [{"eventPattern": "event=streamingstats", "event": "Streaming stats"},{"eventPattern": "state", "event": "State change"},{"eventPattern": "el=detailpage", "event": "Page details"},{"eventPattern": "ptracking", "event": "Pixel track"}]}
 	];
 
 chrome.webRequest.onBeforeRequest.addListener(		//onBeforeRequest	//onSendHeaders
@@ -53,3 +54,8 @@ function Notify(provider, triggeredEvent, xhrData){
    		//console.log(xhrData.url +" : "+ new Date());						
 	});
 }
+
+// Set badge text to 0 when new browser tab is created
+chrome.tabs.onCreated.addListener(function(tab){
+    chrome.browserAction.setBadgeText({text: "", tabId: tab.id});
+})
